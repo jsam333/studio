3785fbea3c1e60533b096ec6a5f27a1b00ab19cb
Jordan Samuel <jsmario321@gmail.com>
2025-04-30 01:41:38 +0000

    made max collection field size smaller

--- src/constants.ts
+++ src/constants.ts
@@ -92,11 +92,11 @@ export const BONUS_GOLD_DECREMENT_INTERVAL = 1000; // Added this export
 // Collection Field
 export const FIELD_INITIAL_HEIGHT_OFFSET = 0;
 export const FIELD_HEIGHT_INCREMENT = 6;
-export const FIELD_MAX_HEIGHT_OFFSET = 45;
+export const FIELD_MAX_HEIGHT_OFFSET = 20;
 export const FIELD_INITIAL_WIDTH_OFFSET = 0;
 export const FIELD_WIDTH_INCREMENT = 8;
-export const FIELD_MAX_WIDTH_OFFSET = 60;
-export const FIELD_SHRINK_RATE_H = 0.09;
+export const FIELD_MAX_WIDTH_OFFSET = 35;
+export const FIELD_SHRINK_RATE_H = 0.1;
 export const FIELD_SHRINK_RATE_W = 0.08;
 export const FIELD_SHRINK_INTERVAL = 16;
 
