69eb1f0de1cd05058e4d6d57dc36ea5b312022bb
Jordan Samuel <jsmario321@gmail.com>
2025-05-09 01:41:40 +0000

    made points easier per level while working on better balancing solution to give player more points

--- src/hooks/useLevelLogic.ts
+++ src/hooks/useLevelLogic.ts
@@ -171,21 +171,21 @@ export function useLevelLogic({
 
         // Set target score (Unchanged)
          let scoreGoal = count;
-        if (currentMode === 'main' && level === 6) { scoreGoal += 10; }
-        if (currentMode === 'main' && level === 7) { scoreGoal += 20; }
-        if (currentMode === 'main' && level === 8) { scoreGoal += 40; }
-        if (currentMode === 'main' && level === 9) { scoreGoal += 80; }
-        if (currentMode === 'main' && level === 10) { scoreGoal += 160; }
-        if (currentMode === 'main' && level === 11) { scoreGoal += 300; }
-        if (currentMode === 'main' && level === 12) { scoreGoal += 500; }
-        if (currentMode === 'main' && level === 13) { scoreGoal += 700; }
-        if (currentMode === 'main' && level === 14) { scoreGoal += 900; }
-        if (currentMode === 'main' && level === 15) { scoreGoal += 1200; }
-        if (currentMode === 'main' && level === 16) { scoreGoal += 1500; }
-        if (currentMode === 'main' && level === 17) { scoreGoal += 1900; }
-        if (currentMode === 'main' && level === 18) { scoreGoal += 2300; }
-        if (currentMode === 'main' && level === 19) { scoreGoal += 2700; }
-        if (currentMode === 'main' && level === 20) { scoreGoal += 3200; }
+        if (currentMode === 'main' && level === 6) { scoreGoal += 5; }
+        if (currentMode === 'main' && level === 7) { scoreGoal += 10; }
+        if (currentMode === 'main' && level === 8) { scoreGoal += 15; }
+        if (currentMode === 'main' && level === 9) { scoreGoal += 25; }
+        if (currentMode === 'main' && level === 10) { scoreGoal += 40; }
+        if (currentMode === 'main' && level === 11) { scoreGoal += 60; }
+        if (currentMode === 'main' && level === 12) { scoreGoal += 80; }
+        if (currentMode === 'main' && level === 13) { scoreGoal += 110; }
+        if (currentMode === 'main' && level === 14) { scoreGoal += 150; }
+        if (currentMode === 'main' && level === 15) { scoreGoal += 200; }
+        if (currentMode === 'main' && level === 16) { scoreGoal += 300; }
+        if (currentMode === 'main' && level === 17) { scoreGoal += 450; }
+        if (currentMode === 'main' && level === 18) { scoreGoal += 650; }
+        if (currentMode === 'main' && level === 19) { scoreGoal += 1000; }
+        if (currentMode === 'main' && level === 20) { scoreGoal += 1500; }
         targetScoreRef.current = scoreGoal;
 
         // Reset other game state elements (Unchanged, except bonus flags)
